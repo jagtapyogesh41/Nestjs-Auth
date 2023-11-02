840611486f4d836769aaee75f92310d42b27d746
jagtapyogesh41 <yjagtap667@gmail.com>
2023-11-02 17:08:23 +0530

    add

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -4,24 +4,18 @@ import { UserPermission } from '../entities/userPermission.entity';
 export class CreateUserDto {
   
   empId: string;
-
   
   userName: string;
 
   password: string;
 
-  
   createdBy?: number;
-
   
   updatedBy?: number;
-
  
   isActive: boolean;
-
   
   userPermission: UserPermission;
-
   
   role: string;
 }
