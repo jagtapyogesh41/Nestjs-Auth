ee9b7524a2fa30fb68ce5a927cd1852574284b51
jagtapyogesh41 <yjagtap667@gmail.com>
2023-11-02 17:06:52 +0530

    add

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,65 +1,27 @@
-import { IsNotEmpty, isNumber } from 'class-validator';
-import { ApiProperty } from '@nestjs/swagger';
 
 import { UserPermission } from '../entities/userPermission.entity';
 
 export class CreateUserDto {
-  @ApiProperty({
-    description: 'user email',
-    required: true,
-    type: String,
-  })
-  @IsNotEmpty()
+  
   empId: string;
 
-  @ApiProperty({
-    description: 'userName',
-    required: true,
-    type: String,
-  })
+  
   userName: string;
 
-  @ApiProperty({
-    description: 'password',
-    required: true,
-    type: String,
-  })
   password: string;
 
-  @ApiProperty({
-    description: 'for now its mandatory',
-    required: true,
-    type: isNumber,
-  })
-  @IsNotEmpty()
+  
   createdBy?: number;
 
-  @ApiProperty({
-    description: 'for now its mandatory',
-    required: true,
-    type: isNumber,
-  })
-  @IsNotEmpty()
+  
   updatedBy?: number;
 
-  @ApiProperty({
-    description: 'isActive',
-    required: true,
-    type: Boolean,
-  })
+ 
   isActive: boolean;
 
-  @ApiProperty({
-    description: 'The user email',
-    required: true,
-    type: Object,
-  })
+  
   userPermission: UserPermission;
 
-  @ApiProperty({
-    description: 'role',
-    required: true,
-    type: String,
-  })
+  
   role: string;
 }
